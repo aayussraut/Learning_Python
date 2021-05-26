44725c729b9379f34469c90f85d72f72917f7191
Aayush Raut <aayussraut.ar@gmial.com>
2021-05-26 14:00:17 +0545

    Part of PythonP3.ipynb

---
+++ car.py
@@ -0,0 +1,24 @@
+class Car():
+    """ A simple attempt to represent a car."""
+
+    def __init__(self,make,model,year):
+        self.make = make
+        self.model=model
+        self.year=year
+        self.odometer_reading =0
+    
+    def get_descriptive(self):
+        long_name=str(self.year)+" "+self.make+" "+self.model
+        return long_name
+    
+    def read_odometer(self):
+        print("this car has "+str(self.odometer_reading)+" miles on it.")
+    
+    def update_odometer(self,mileage):
+        if mileage>=self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You cant roll back on odometer.")
+    
+    def increment_odometer(self,miles):
+        self.odometer_reading+=miles
